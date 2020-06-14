55fe626da28dc3bcb249994a8f3c95c0e4f6db5a
nikaalin <lina.y2018@ya.ru>
2020-06-14 14:50:57 +0300

    Fixed html appearance

--- app/src/main/java/com/example/rssnews/ui/main/BindingHelper.kt
+++ app/src/main/java/com/example/rssnews/ui/main/BindingHelper.kt
@@ -1,5 +1,9 @@
 package com.example.rssnews.ui.main
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.databinding.BindingAdapter
 import androidx.databinding.BindingConversion
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -17,3 +21,12 @@ fun convertDateTimeToString(dateTime: LocalDateTime): String =
 @BindingConversion
 fun convertListToString(list: List<String>): String =
     list.reduce { total, next -> "$total, $next" }.trimEnd(',', ' ')
+
+@BindingAdapter("html")
+fun TextView.setHtml(htmlBody: String) {
+    this.text = HtmlCompat.fromHtml(htmlBody, HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_LIST)
+    this.movementMethod = LinkMovementMethod.getInstance()
+}
+
+
+
